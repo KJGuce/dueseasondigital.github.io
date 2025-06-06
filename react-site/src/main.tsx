6487da32d94a53cd276cf3922a1c28b7eb64b8ef
KayJayGoosay <kjguce@gmail.com>
2025-06-06 03:31:26 -0400

    Update styling to match local version

--- react-site/src/main.tsx
+++ react-site/src/main.tsx
@@ -8,6 +8,7 @@ import "@fontsource/nunito/700.css";
 import "@fontsource/nunito/800.css";
 import "./index.css";
 import "./App.css";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
