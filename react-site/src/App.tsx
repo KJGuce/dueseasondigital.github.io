6f0788462c04830004bcbd896800b5eda3e0a8e8
KayJayGoosay <kjguce@gmail.com>
2025-06-06 01:36:24 -0400

    Remove Router basename for GitHub Pages

--- react-site/src/App.tsx
+++ react-site/src/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/dueseasondigital.github.io">
+    <Router>
       <div className="min-h-screen">
         <Navbar />
         <main className="pt-16">
