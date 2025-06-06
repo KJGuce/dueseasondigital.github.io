f0ab050d8f30273a849ac8b374ba3ea28242538f
KayJayGoosay <kjguce@gmail.com>
2025-06-06 02:58:03 -0400

    Fix routing for custom domain

--- react-site/src/App.tsx
+++ react-site/src/App.tsx
@@ -10,7 +10,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/dueseasondigital.github.io">
+    <Router>
       <div className="app">
         <Navbar />
         <main>
--- react-site/vite.config.ts
+++ react-site/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dueseasondigital.github.io/",
+  base: "/",
   build: {
     outDir: "dist",
     rollupOptions: {
