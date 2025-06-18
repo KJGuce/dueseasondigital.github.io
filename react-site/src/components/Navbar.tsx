4571196909f1be32bce87df11ef165a8367244fb
KayJayGoosay <kjguce@gmail.com>
2025-06-18 03:38:00 -0400

    chore: remove unused useLocation import from Navbar.tsx

--- react-site/src/components/Navbar.tsx
+++ react-site/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import styles from "./Navbar.module.css";
 
